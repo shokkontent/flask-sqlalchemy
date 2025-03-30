f267d8e27623441818f82d47f675e151004bbb79
Dima <grmgorer@gmail.com>
2025-03-30 22:48:24 +0300

    сайт работующий

---
+++ create.py
@@ -0,0 +1,51 @@
+import datetime
+
+from data import db_session
+from data.users import User
+
+
+db_session.global_init("db/blogs.db")
+db_sess = db_session.create_session()
+
+user = User()
+user.surname = "Scott"
+user.name = "Ridley"
+user.age = "21"
+user.position = "captain"
+user.speciality = "research engineer"
+user.address = "module_1"
+user.email = "scott_chief@mars.org"
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.surname = "Scgfott"
+user.name = "Ringdley"
+user.age = "22"
+user.position = "captabfin"
+user.speciality = "resebfbarch bgenginbfeer"
+user.address = "module_2"
+user.email = "scottvfdvfdchief@mars.org"
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.surname = "Scotsst"
+user.name = "Ridgfley"
+user.age = "25"
+user.position = "captbfin"
+user.speciality = "researbfch engineer"
+user.address = "module_3"
+user.email = "scobftt_bfchiefbgf@mars.org"
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+
+'''user = db_sess.query(User).filter(User.id == 1).first()
+print(user)
+user.name = "Пользователь 1"
+user.created_date = datetime.datetime.now()
+db_sess.commit()'''
